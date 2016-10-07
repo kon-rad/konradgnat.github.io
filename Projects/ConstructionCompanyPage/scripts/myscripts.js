f787d0f36c5534849e8eae27180ce3351c5d23b9
Fernando1Borges <centraloro@gmail.com>
2016-10-06 20:10:47 -0500

    Create ABC Construction Inc page

---
+++ Projects/ConstructionCompanyPage/scripts/myscripts.js
@@ -0,0 +1,35 @@
+'use strict';
+
+$(document).ready(function(){
+  $(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
+
+  $("#mycarousel").carousel( { interval: 500 } );
+
+  $("#carouselPause").click(function(){
+    $("#mycarousel").carousel('pause');
+  });
+  $("#carouselPlay").click(function() {
+    $("#mycarousel").carousel('cycle');
+  });
+
+
+  /* manually opens reservation modal */
+  $('#open-reservation-modal').click(function(){
+    $("#reservationModal").modal('show');
+  })
+  /* manually hides reservation modal */
+  $('.hide-reservation-modal').click(function(){
+    $('#reservationModal').modal('hide');
+  });
+  /* manually opens login modal */
+  $('#open-login-modal').click(function(){
+    $("#loginModal").modal('show');
+  })
+  /* manually hides login modal */
+  $('.hide-login-modal').click(function(){
+    $('#loginModal').modal('hide');
+  });
+});
