a0d40536bc49ced8df4c562ded9dd69862c2f399
Fernando1Borges <centraloro@gmail.com>
2016-11-14 22:31:31 -0600

    Add day 14, install custom blog theme to wordpress

---
+++ Build_Day/Build_Day_14-27/blogTheme/content.php
@@ -0,0 +1,69 @@
+
+
+
+    <ul class="blog-items">
+
+      <li class="item">
+        <div class="item-inner">
+          <a href="item001.html">
+        <img src="<?php bloginfo('template_directory');?>/img/sample001.png" alt="sample blog image"
+          class="img-responsive">
+        </a>
+          <div class="item-desc">
+        <h3><?php the_title(); ?></h3>
+        <p><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+        <p><?php the_content(); ?>
+          <a href="item001.html">Read more...</a></p>
+      </div>
+    </div>
+      </li>
+
+      <li class="item">
+        <div class="item-inner">
+          <a href="item001.html">
+        <img src="<?php bloginfo('template_directory');?>/img/sample001.png" alt="sample blog image"
+          class="img-responsive">
+        </a>
+          <div class="item-desc">
+        <h3>The Release of Letraset Sheets</h3>
+        <p>There are many variations of passages of Lorem Ipsum
+          available, but the majority have suffered alteration in
+          some form, by injected humour, or randomised words which
+          don't look even slightly believable. If you are going to
+          use a passage of Lorem Ipsum, you need to be sure there
+          isn't anything embarrassing hidden in the middle of text...
+          <a href="item001.html">Read more...</a></p>
+      </div>
+    </div>
+      </li>
+      <li class="item">
+        <div class="item-inner">
+          <a href="item002.html">
+        <img src="<?php bloginfo('template_directory');?>/img/sample002.png" alt="sample blog image"
+         class="img-responsive">
+       </a>
+         <div class="item-desc">
+        <h3>Many Desktop Publishing</h3>
+        <p>Richard McClintock, a Latin professor at Hampden-Sydney
+          College in Virginia, looked up one of the more obscure
+          Latin words, consectetur, from a Lorem Ipsum passage, and
+          going through the cites of the word in classical literature,
+           discovered the undoubtable source...<a href="item002.html">Read more...</a></p>
+      </div>
+  </div>
+      </li>
+      <li class="item">
+        <div class="item-inner">
+          <a href="item003.html">
+        <img src="<?php bloginfo('template_directory');?>/img/sample003.png" alt="sample blog image"
+        class="img-responsive">
+      </a>
+        <div class="item-desc">
+        <h3>Where can I get some?</h3>
+        <p>The standard chunk of Lorem Ipsum used since the 1500s is
+          reproduced below for those interested. Sections 1.10.32 and
+          1.10.33 from "de Finibus Bonorum et Malorum" by
+          Cicero...<a href="item003.html">Read more...</a></p>
+      </div>
+  </div>
+      </li>
