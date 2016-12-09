ebdb44d1fcb2674d2dbbe8c6b0785093be8b3e34
Fernando1Borges <centraloro@gmail.com>
2016-12-08 21:31:38 -0600

    Change navigation bar hamburger icon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
 var top_header = '';
 $(document).ready(function(){
   top_header = $('.parallax-scroll');
 });
 $(window).scroll(function () {
-  var st = $(window).scrollTop(); 
+  var st = $(window).scrollTop();
   top_header.css({'background-position':'50%' + (st*.3)+"px"});
 });
