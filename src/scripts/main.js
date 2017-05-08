adfd6aa48da037d421244f123ac1e71ec7067626
konradgnat <konradmgnat@gmail.com>
2017-05-08 18:42:33 -0500

    remove parallax, make title font smaller

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,12 @@ function myFunction(x) {
     $(".collapse").toggle();
 }
 
-var top_header = '';
-$(document).ready(function(){
-  top_header = $('.parallax-scroll');
-});
-$(window).scroll(function () {
-  var st = $(window).scrollTop();
-  top_header.css({'background-position':'50%' + (st*.3)+"px"});
-});
+// var top_header = '';
+// $(document).ready(function(){
+//   top_header = $('.parallax-scroll');
+// });
+
+// $(window).scroll(function () {
+//   var st = $(window).scrollTop();
+//   top_header.css({'background-position':'50%' + (st*.3)+"px"});
+// });
