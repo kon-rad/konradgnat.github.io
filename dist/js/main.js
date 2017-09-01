94c0d0c72338bf0ebc749368c8835f112dba6169
konradgnat <konradmgnat@gmail.com>
2017-08-31 21:23:12 -0500

    Add slider test

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,57 +1,70 @@
-"use strict";
+'use strict';
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-function myFunction(x) {
-  x.classList.toggle("change");
-  $(".collapse").toggle();
-}
+requirejs(['jquery-3.2.1.min', 'swiper.jquery.min', 'swiper.min', 'swiper.jquery.umd.min'], function () {
 
-if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype && !('isIntersecting' in IntersectionObserverEntry.prototype)) {
-
-  Object.defineProperty(window.IntersectionObserverEntry.prototype, 'isIntersecting', {
-    get: function get() {
-      return this.intersectionRatio > 0;
-    }
+  var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    paginationClickable: true,
+    spaceBetween: 30,
+    slidesPerView: 1
   });
-}
 
-var webp = false;
+  function myFunction(x) {
+    x.classList.toggle("change");
+    $(".collapse").toggle();
+  }
 
-if ($('html').hasClass('webp')) webp = true;
+  if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype && !('isIntersecting' in IntersectionObserverEntry.prototype)) {
 
-console.log(webp);
+    Object.defineProperty(window.IntersectionObserverEntry.prototype, 'isIntersecting', {
+      get: function get() {
+        return this.intersectionRatio > 0;
+      }
+    });
+  }
 
-var imgElements = [].concat(_toConsumableArray(document.querySelectorAll('.lazyload')));
+  var webp = false;
 
-var observer = new IntersectionObserver(onChange, {
-  threshold: [0.0]
+  if ($('html').hasClass('webp')) webp = true;
+
+  console.log(webp);
+
+  var imgElements = [].concat(_toConsumableArray(document.querySelectorAll('.lazyload')));
+
+  var observer = new IntersectionObserver(onChange, {
+    threshold: [0.0]
+  });
+
+  function onChange(changes) {
+
+    changes.forEach(function (change) {
+
+      if (change.isIntersecting) {
+
+        change.target.classList.add('visible');
+        if (!webp) change.target.src = change.target.dataset.src;else change.target.src = change.target.dataset.webp;
+
+        observer.unobserve(change.target);
+      }
+    });
+  }
+
+  var createObserver = function createObserver() {
+
+    imgElements.forEach(function (img) {
+
+      observer.observe(img);
+    });
+  };
+
+  window.addEventListener("load", function () {
+    createObserver();
+  }, false);
+
+  console.timeEnd("start_to_end");
 });
-
-function onChange(changes) {
-
-  changes.forEach(function (change) {
-
-    if (change.isIntersecting) {
-
-      change.target.classList.add('visible');
-      if (!webp) change.target.src = change.target.dataset.src;else change.target.src = change.target.dataset.webp;
-
-      observer.unobserve(change.target);
-    }
-  });
-}
-
-var createObserver = function createObserver() {
-
-  imgElements.forEach(function (img) {
-
-    observer.observe(img);
-  });
-};
-
-window.addEventListener("load", function () {
-  createObserver();
-}, false);
-
-console.timeEnd("start_to_end");
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,64 +1,84 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-    $(".collapse").toggle();
-}
+requirejs([
+  'jquery-3.2.1.min',
+  'swiper.jquery.min',
+  'swiper.min',
+  'swiper.jquery.umd.min'
+  ], function() {
 
+    var swiper = new Swiper('.swiper-container', {
+      pagination: '.swiper-pagination',
+          // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+      paginationClickable: true,
+      spaceBetween: 30,
+      slidesPerView: 1
+    });
 
-if ('IntersectionObserver' in window &&
-  'IntersectionObserverEntry' in window &&
-  'intersectionRatio' in window.IntersectionObserverEntry.prototype &&
-  !('isIntersecting' in IntersectionObserverEntry.prototype)) {
-
-  Object.defineProperty(window.IntersectionObserverEntry.prototype, 'isIntersecting', {
-    get: function () {
-      return this.intersectionRatio > 0
+    function myFunction(x) {
+      x.classList.toggle("change");
+      $(".collapse").toggle();
     }
-  })
-}
 
-let webp = false;
 
-if($('html').hasClass('webp')) webp = true;
+    if ('IntersectionObserver' in window &&
+      'IntersectionObserverEntry' in window &&
+      'intersectionRatio' in window.IntersectionObserverEntry.prototype &&
+      !('isIntersecting' in IntersectionObserverEntry.prototype)) {
 
-console.log(webp);
+      Object.defineProperty(window.IntersectionObserverEntry.prototype, 'isIntersecting', {
+        get: function () {
+          return this.intersectionRatio > 0
+        }
+      })
+  }
 
-const imgElements = [ ...document.querySelectorAll( '.lazyload' ) ];
+  let webp = false;
 
-let observer = new IntersectionObserver( onChange, {
-  threshold: [ 0.0 ],
-});
+  if($('html').hasClass('webp')) webp = true;
 
-function onChange( changes ) {
+  console.log(webp);
 
-  changes.forEach( change => {
+  const imgElements = [ ...document.querySelectorAll( '.lazyload' ) ];
 
-    if ( change.isIntersecting ) {
-
-      change.target.classList.add( 'visible' );
-      if(!webp)
-      	change.target.src = change.target.dataset.src;
-      else
-      	change.target.src = change.target.dataset.webp;
-
-      observer.unobserve( change.target );
-    }
+  let observer = new IntersectionObserver( onChange, {
+    threshold: [ 0.0 ],
   });
-}
+
+  function onChange( changes ) {
+
+    changes.forEach( change => {
+
+      if ( change.isIntersecting ) {
+
+        change.target.classList.add( 'visible' );
+        if(!webp)
+          change.target.src = change.target.dataset.src;
+        else
+          change.target.src = change.target.dataset.webp;
+
+        observer.unobserve( change.target );
+      }
+    });
+  }
 
 
-const createObserver = function () {
+  const createObserver = function () {
 
-  imgElements.forEach( img => {
+    imgElements.forEach( img => {
 
-    observer.observe( img );
-  });
-};
+      observer.observe( img );
+    });
+  };
 
 
-window.addEventListener("load", () => {
+  window.addEventListener("load", () => {
     createObserver();
-}, false);
+  }, false);
 
 
 
-console.timeEnd("start_to_end");
+  console.timeEnd("start_to_end");
+})
+
+
