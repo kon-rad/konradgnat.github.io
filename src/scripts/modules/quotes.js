8fd1fe2e915ecbd1943fa10ff24bac121f1e8173
konradgnat <konradmgnat@gmail.com>
2018-09-18 07:16:22 -0500

    rm modal

--- src/scripts/modules/quotes.js
+++ src/scripts/modules/quotes.js
@@ -1,52 +1,42 @@
 'use strict';
 
-define(['jquery', 'mainSlider'], function($, mainSlider) {
+define(['jquery', 'mainSlider'], function ($, mainSlider) {
 
   /* Generate New Quote function */
 
-    function newQuote() {
-	    $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
-			function(data) {
-				if (data === null) {
-					console.log('Something went wrong');
-				} else {
-				    var html = "";
-				    var authorHtml = "";
-				    html += data.quoteText;
-				    authorHtml += data.quoteAuthor;
-				    if (authorHtml == "")
-				    	authorHtml = "Author Unknown";
-				   	$('#quote').html(html);
-				  	$('#author').html(authorHtml);	    
-				}
-			});    	
-    }
+  function newQuote() {
+    $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
+      function (data) {
+        if (data === null) {
+          console.log('Something went wrong');
+        } else {
+          var html = "";
+          var authorHtml = "";
+          html += data.quoteText;
+          authorHtml += data.quoteAuthor;
+          if (authorHtml == "")
+            authorHtml = "Author Unknown";
+          $('#quote').html(html);
+          $('#author').html(authorHtml);
+        }
+      });
+  }
 
+  newQuote();
+
+  var getNewQuote = document.getElementById('newQuote');
+
+
+  mainSlider.startAutoplay();
+  document.getElementById('website_wrapper').style.visibility = 'visible';
+  getNewQuote.onclick = function () {
     newQuote();
+  };
 
-	var modal = document.getElementById('modal');
-
-    modal.style.display = "block";
-
-	var span = document.getElementById("modalClose");
-
-	var getNewQuote = document.getElementById('newQuote');
-
-	span.onclick = function() {
-	    modal.style.display = "none";
-	    mainSlider.startAutoplay(); 
-	    document.getElementById('website_wrapper').style.visibility = 'visible';
-	}
-
-	getNewQuote.onclick = function() {
-	    newQuote();
-	}
-
-	window.onclick = function(event) {
-	    if (event.target == modal) {
-	        modal.style.display = "none";
-		    mainSlider.startAutoplay(); 
-		    document.getElementById('website_wrapper').style.visibility = 'visible';
-	    }
-	}
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      mainSlider.startAutoplay();
+      document.getElementById('website_wrapper').style.visibility = 'visible';
+    }
+  }
 });
