12b8b08a93422a3b1501580edfd2fe8218160472
konradgnat <konradmgnat@gmail.com>
2019-07-08 21:09:39 -0500

    improve code

--- src/scripts/modules/quotes.js
+++ src/scripts/modules/quotes.js
@@ -1,34 +1,22 @@
 'use strict';
 
 define(['jquery', 'mainSlider'], function ($, mainSlider) {
-
   /* Generate New Quote function */
   function newQuote() {
     $.getJSON('http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
       function (data) {
-        if (data === null) {
+        if (!data) {
           console.log('Something went wrong');
           return;
         }
 
-        var html = "";
-        var authorHtml = "";
-        html += data.quoteText;
-        authorHtml += data.quoteAuthor;
-        if (authorHtml == "")
-          authorHtml = "Author Unknown";
-        $('#quote').html(html);
-        $('#author').html(authorHtml);
+        $('#quote').html(data.quoteText);
+        $('#author').html(data.quoteAuthor ? data.quoteAuthor : 'Author Unknown');
       });
   }
 
   newQuote();
-
-  var getNewQuote = document.getElementById('newQuote');
-
   mainSlider.startAutoplay();
   document.getElementById('website_wrapper').style.visibility = 'visible';
-  getNewQuote.onclick = function () {
-    newQuote();
-  };
+  document.getElementById('newQuote').onclick = newQuote;
 });
