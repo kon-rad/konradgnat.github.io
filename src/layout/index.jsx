011c8851493f3260a426723aebd7720b2aae1c25
konradgnat <konradmgnat@gmail.com>
2019-07-12 22:06:12 -0500

    add material css

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 import "./index.css";
+import 'materialize-css/dist/css/materialize.min.css';
 
 export default class MainLayout extends React.Component {
   render() {
@@ -10,7 +11,19 @@ export default class MainLayout extends React.Component {
       <div>
         <Helmet>
           <meta name="description" content={config.siteDescription} />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         </Helmet>
+        <nav className="blue lighten-3">
+          <div className="nav-wrapper container">
+            <ul className="left">
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/portfolio">Portfolio</a></li>
+              <li><a href="/blog">Blog</a></li>
+            </ul>
+          </div>
+        </nav>
+
         {children}
       </div>
     );
