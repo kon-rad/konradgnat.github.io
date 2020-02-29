362ee71aadc4dfcc7961d2bf30ec9db7025dd9f9
konradgnat <konradmgnat@gmail.com>
2020-02-29 16:26:20 -0600

    add blog

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -2,27 +2,48 @@ import React from 'react';
 
 export default () => {
   return (
-    <nav id="home" className="nav navbar-default navbar-fixed-top" role="navigation">
+    <nav
+      id="home"
+      className="nav navbar-default navbar-fixed-top"
+      role="navigation"
+    >
       <div className="container">
         <div className="navbar-header">
-          <div id="menuToggle" className="container-button" type="button"
-               className="navbar-toggle collapsed" data-toggle="collapse"
-               data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <div
+            id="menuToggle"
+            className="container-button"
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+            aria-controls="navbar"
+          >
             <div className="bar1"></div>
             <div className="bar2"></div>
             <div className="bar3"></div>
           </div>
-          <a className="navbar-brand" href="#home"><i className="fa fa-code" aria-hidden="true"></i></a>
+          <a className="navbar-brand" href="#home">
+            <i className="fa fa-code" aria-hidden="true"></i>
+          </a>
         </div>
         <div id="navbar" className="navbar-collapse collapse">
           <ul className="nav navbar-nav">
-            <li><a href="#about">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="./blog.html">Blog</a></li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="./blog">Blog</a>
+            </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 };
--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -3,10 +3,84 @@ import { Link } from 'react-router-dom';
 
 function Blog() {
   return (
-    <div className="About">
-      <h1>About page</h1>
-      <Link to="/">Go to Home page</Link>
-    </div>
+    <section className="blog">
+      <div className="jumbotron_image__blog-header"></div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-8 col-sm-offset-2">
+            <div className="page-header">
+              <h1>Articles</h1>
+            </div>
+            <div className="blog_text">
+              <section className="blog_links">
+                <div className="well well-lg">
+                  <p>
+                    <a href="blog-redux-saga.html">
+                      What is Redux-Saga{' '}
+                      <span className="float_right">December 3rd, 2017</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-jest.html">
+                      How to test a react app with Jest
+                      <span className="float_right">December 10th, 2017</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-heaps.html">
+                      Min and max heaps in Python3
+                      <span className="float_right">December 17th, 2017</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-trie.html">
+                      The Trie Data Structure in Python3
+                      <span className="float_right">December 25th, 2017</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-bubble-and-quicksort.html">
+                      Bubble Sort and Quicksort Algorithms in Python3
+                      <span className="float_right">January 2nd, 2018</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-longest-common-subsequence.html">
+                      Longest Common Subsequence Problem
+                      <span className="float_right">January 14th, 2018</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-merge-sort.html">
+                      Merge Sort Algorithm
+                      <span className="float_right">January 28th, 2018</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-heaps-algorithm.html">
+                      Heap's Algorithm
+                      <span className="float_right">February 15th, 2018</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-functional-programming.html">
+                      What is Functional Programming?
+                      <span className="float_right">March 30th, 2019</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="blog-git-commands.html">
+                      Helpful git Commands
+                      <span className="float_right">September 30th, 2019</span>
+                    </a>
+                  </p>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
