9ce382d932c0773f659175f45bacf4e009ca8e81
konradgnat <konradmgnat@gmail.com>
2020-03-07 19:30:30 -0600

    add blog

--- client/src/components/blogBlurb/index.js
+++ client/src/components/blogBlurb/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BlogBlurb = post => {
+const BlogBlurb = ({ post }) => {
+  console.log('post', post);
   return (
     <p>
+      start
       <Link to={post.slug || '#'}>
         {post.title} <span className="float_right">{post.date}</span>
       </Link>
+      end
     </p>
   );
 };
