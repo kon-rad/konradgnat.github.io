ac1caf73a41deeb3c3709d98c4b86d36aedf6b7d
konradgnat <konradmgnat@gmail.com>
2020-12-15 20:18:29 -0600

    add new curve line design, copy to clipboard

--- src/index.js
+++ src/index.js
@@ -6,5 +6,3 @@ import './scripts/vendors/swiper.min';
 import './scripts/modules/hamburgerMenu';
 import './scripts/modules/mainSlider';
 import './scripts/modules/quotes';
-
-console.log('helloasd');
--- src/scripts/modules/quotes.js
+++ src/scripts/modules/quotes.js
@@ -6,13 +6,32 @@ const newQuote = () => {
     'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
     (data) => {
       if (!data) {
-        console.error('Error: Could not fetch quote from forismatic api.');
+        console.error(
+          'Error: Could not fetch quote from forismatic api.',
+        );
         return;
       }
 
       $('#quote').html(data.quoteText);
-      $('#author').html(data.quoteAuthor ? data.quoteAuthor : 'Author Unknown');
-    });
+      $('#author').html(
+        data.quoteAuthor ? data.quoteAuthor : 'Author Unknown',
+      );
+    },
+  );
+};
+
+const copyQuote = () => {
+  var quote = $('#quote').text();
+  var author = $('#author').text();
+  const copy = `"${quote}" - ${author}`;
+  navigator.clipboard.writeText(copy).then(
+    function () {
+      // console.log('Async: Copying to clipboard was successful!');
+    },
+    function (err) {
+      console.error('Async: Could not copy text: ', err);
+    },
+  );
 };
 
 $(document).ready(() => {
@@ -21,6 +40,8 @@ $(document).ready(() => {
   }
   newQuote();
 
-  document.getElementById('website_wrapper').style.visibility = 'visible';
+  document.getElementById('website_wrapper').style.visibility =
+    'visible';
   document.getElementById('newQuote').onclick = newQuote;
+  document.getElementById('copyQuoteToClipboard').onclick = copyQuote;
 });
