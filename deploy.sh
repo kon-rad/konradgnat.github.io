d80ab0de2f34fa438fcf677da5fc90811e2a2a1d
konradgnat <konradmgnat@gmail.com>
2020-12-28 20:55:10 -0600

    add deploy bash script, remove sql backup

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd konradgnat_com/
+sudo git reset --hard
+sudo git pull
+cd app/client
+sudo npm install
+sudo npm run build
+cd ../konradgnat
+
+# start virtual env, update requirements, rebuild app
+
+virtualenv .
+source bin/activate
+
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic
+
+# restarts gunicorn, resets .sock
+
+sudo systemctl daemon-reload
+sudo systemctl restart konradgnat
+deactivate
