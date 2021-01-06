17a0162a7ea9eefd9b13dd3b4d19bc757f8c3fad
konradgnat <konradmgnat@gmail.com>
2021-01-05 19:43:26 -0600

    add quotes data pull script

--- app/konradgnat/quotesApi/pullData.py
+++ app/konradgnat/quotesApi/pullData.py
@@ -1,10 +1,38 @@
 # importing the requests library 
-import requests 	import requests 
-import json	import json
+import requests 
+import json
+from secret import * 
+import time
 
-print(data["quoteText"])	  print(data["quoteText"])
-post_data = {"text": data["quoteText"], "author": data["quoteAuthor"] }	  
-print("post data: ", post_data)	  
-print("post data: ", post_data)
-r_post = requests.post('https://konradgnat.com/quotes-api/quotes/', json=post_data, auth=('<>', '<>'))
-print("post status code: ", r_post.status_code)	 
+# api-endpoint 
+URL = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
+
+uploaded_count = 0
+
+for n in range(0, 100):
+  print("n", n)
+  try:
+    r = requests.get(url = URL)
+    print("content: ", r.content)
+    # dump = json.dumps(r.content)
+    # print("dump", dump)
+    # .replace(r'\', r'\\')
+    data = json.loads(r.content)
+    post_data = {"text": data["quoteText"], "author": data["quoteAuthor"] }
+  except Exception as e:
+    print('Failed to get quote: '+ str(e))
+    continue
+
+  print("post data: ", post_data)
+  r_post = requests.post('https://konradgnat.com/quotes-api/quotes/', json=post_data, auth=(ADMIN_USER, ADMIN_PASSWORD))
+  print("post status code: ", r_post.status_code)
+  if r_post.status_code != 201:
+    print('post was unsuccessful')
+    with open('quotesNotUploadedSuccessfully.json', 'w') as outfile:
+        json.dump(post_data, outfile)
+  else:
+    print('post was successful')
+    uploaded_count += 1
+  time.sleep(3)
+print("posted ", update_count, " number of quotes")
+  
